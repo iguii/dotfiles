af1a5adaffef408340caf5524145f3216ec2c925
iguiIllanes <igui10082002@gmail.com>
2023-07-20 19:17:51 -0400

    Add keymap for lazygit.nvim

--- nvim/lua/ignacioillanes/core/keymaps.lua
+++ nvim/lua/ignacioillanes/core/keymaps.lua
@@ -38,3 +38,6 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- search for stri
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- lazygit.nvim
+keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
